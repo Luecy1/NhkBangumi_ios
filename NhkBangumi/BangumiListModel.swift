c21a539346fc5db63ed0ed72d8345440e834b7d8
youhei <oonuma617@gmail.com>
2019-05-20 16:57:34 +0900

    外側のコミット

---
+++ NhkBangumi/BangumiListModel.swift
@@ -0,0 +1,15 @@
+//
+//  BangumiListModel.swift
+//  NhkBangumi
+//
+//  Created by 大沼　陽平 on 2019/05/20.
+//  Copyright © 2019 Youhei oonuma. All rights reserved.
+//
+import RxSwift
+
+class BangumiListModel {
+
+    func getBangumiList() {
+
+    }
+}
---
+++ NhkBangumi/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  NhkBangumi
+//
+//  Created by 大沼　陽平 on 2019/05/20.
+//  Copyright © 2019 Youhei oonuma. All rights reserved.
+//
+import RxSwift
+
+class HomeViewModel {
+    
+    
+//    let bangumiItem: Observable<[(title: String, subtitle: String)]>
+
+}
